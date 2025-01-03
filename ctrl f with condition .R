6183619ffccc45bc2990dc74576434a0253169a3
Seungyeon <155028735+SeungYeon-H@users.noreply.github.com>
2025-01-02 21:28:40 -0500

    Update ctrl f with condition .R

--- ctrl f with condition .R
+++ ctrl f with condition .R
@@ -1,4 +1,9 @@
-#not using gsub
-Change the 'Han River' element included in data$region, for those rows whose Site(Kor) column is "MC"
-data$region[data$`Site(Kor)` == "MC" & data$region == "Han River"] <- "Mongchon"
+### ctrl + F with multiple conditions 
+#Change the 'Han River' element included in data$region, for those rows whose Site(Kor) column is "MC"
+data$region[data$`Site(Kor)` == "MC" & data$region == "Han River"] <- "Mongchon" # not using gsub
+#or
+data$region <- with(data, gsub("Han River", "Mongchon", region, fixed = TRUE, 
+                               ignore.case = FALSE)[`Site(Kor)` == "MC"]) #using gsub
  
+#simply change "Han River into "Mongchon"
+data$region <- gsub("Han River", "Mongchon", data$region)
