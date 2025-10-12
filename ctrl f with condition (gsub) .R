0f1e47c5dd6cf99f5c1354cde7f46c90d0db31a3
Seungyeon <155028735+SeungYeon-H@users.noreply.github.com>
2025-10-12 10:18:38 -0400

    Update and rename ctrl f with condition .R to ctrl f with condition (gsub) .R

--- ctrl f with condition (gsub) .R
+++ ctrl f with condition (gsub) .R
@@ -1,3 +1,6 @@
+#simply change "Han River into "Mongchon"
+data$region <- gsub("Han River", "Mongchon", data$region)
+
 ### ctrl + F with multiple conditions 
 #Change the 'Han River' element included in data$region, for those rows whose Site(Kor) column is "MC"
 data$region[data$`Site(Kor)` == "MC" & data$region == "Han River"] <- "Mongchon" # not using gsub
@@ -5,5 +8,3 @@ data$region[data$`Site(Kor)` == "MC" & data$region == "Han River"] <- "Mongchon"
 data$region <- with(data, gsub("Han River", "Mongchon", region, fixed = TRUE, 
                                ignore.case = FALSE)[`Site(Kor)` == "MC"]) #using gsub
  
-#simply change "Han River into "Mongchon"
-data$region <- gsub("Han River", "Mongchon", data$region)
