6e42ff707f556bcfa43fa2958d7ee9b51fbb6553
Seungyeon <155028735+SeungYeon-H@users.noreply.github.com>
2025-10-16 16:22:23 -0400

    Update gives a serial number from 1.R

--- gives a serial number from 1.R
+++ gives a serial number from 1.R
@@ -2,3 +2,6 @@ rowid_to_column("a")  #function that gives a serial number from 1 to the left of
 
 #other pathways:
 data[ , "id" ] <- 1:nrow(data) 
+
+#other pathways:
+sample<-bind_cols(sample1,sample2) %>%   mutate(row=row_number())
